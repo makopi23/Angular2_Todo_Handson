1670c56e7cf763f6c87cc72411a5511f4f79d7cc
makopi23 <kipersonalpc@gmail.com>
2016-06-22 20:14:12 +0900

    step2 completed.

--- src/components/step.2/template/app.ts
+++ src/components/step.2/template/app.ts
@@ -37,16 +37,18 @@ export class AppTemplate
 
   ngOnInit(): void {
     // TODO: 1. todo及びtodosの初期化を行います
-
+    this.todo = new Todo;
+    this.todos = [];
   }
 
   public onSubmit(): void {
     // TODO: 2. todoをtodosに保存し、todoを初期化します
-
+    this.todos.push(this.todo);
+    this.todo = new Todo;
   }
 
   public onDelete(index: number): void {
     // TODO: 3. todosから該当する行番号(index)でデータを削除します
-
+    this.todos.splice(index, 1);
   }
 }
